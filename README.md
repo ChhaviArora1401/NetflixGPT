5b489fed4001e6c0d6424e474616dad1be30318e
Chhavi Arora <chhaviarora1401@gmail.com>
2025-05-15 15:47:47 +0530

    initial

---
+++ README.md
@@ -0,0 +1,19 @@
+# Netflix GPT
+- Vite React App
+- Configured TailwindCSS
+- Github Repo
+
+# Features: 
+- Login/Sign Up
+  - SignIn / Sign Up form
+- Browse page
+  - Header
+  - main movie
+    - trailer in Bg
+    - title & description
+    - Movie suggestions
+      - MovieList * N
+
+- NetflixGPT
+  - Search Bar
+  - Movie Suggestions 
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Namaste Everyone, lets's build Netflix GPT
+      </div>
+    </>
+  )
+}
+
+export default App
